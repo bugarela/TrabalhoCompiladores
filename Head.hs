0848d5ccbe5c32054f86dd290cab139335ca9521
Gabriela Moreira <gabrielamoreira05@gmail.com>
2018-04-26 16:56:25 -0300

    Lexico e Sintatico em progresso

--- Head.hs
+++ Head.hs
@@ -1,21 +1,42 @@
 module Head where
 
+data Literal = String | Integer
+
+type Identificador = String
+
 --data Programa = Completo ListaFuncoes BlocoPrincipal
   --            | Main BlocoPrincipal
 
-data OperadorAritmetico = Multiplicacao | Divisao | Adicao | Subtracao deriving (Show)
+type ListaParametros = [Parametro]
+data Parametro = ParametroLiteral Literal
+               | ParametroExpressao ExpressaoAritmetica
 
-data ExpressaoAritmetica = Binario OperadorAritmetico ExpressaoAritmetica ExpressaoAritmetica
+data ExpressaoAritmetica = Multiplicacao ExpressaoAritmetica ExpressaoAritmetica
+                         | Divisao ExpressaoAritmetica ExpressaoAritmetica
+                         | Adicao ExpressaoAritmetica ExpressaoAritmetica
+                         | Subtracao ExpressaoAritmetica ExpressaoAritmetica
                          | Neg ExpressaoAritmetica
                          | AA
                          deriving (Show)
 
-data ExpressaoLogica = Ou ExpressaoLogica ExpressaoLogica
-                     | E ExpressaoLogica ExpressaoLogica
+data ExpressaoLogica = E ExpressaoLogica ExpressaoLogica
+                     | Ou ExpressaoLogica ExpressaoLogica
                      | Nao ExpressaoLogica
                      | Verdadeiro
                      | Falso
+                     deriving (Show)
 
-instance Show ExpressaoLogica where
-   show (E a b) = "(" ++ show a ++ " && " ++ show b ++ ")"
-   show Verdadeiro = "Verdadeiro"
+data ExpressaoRelacional = Maior ExpressaoAritmetica ExpressaoAritmetica
+                         | Menor ExpressaoAritmetica ExpressaoAritmetica
+                         | MaiorIgual ExpressaoAritmetica ExpressaoAritmetica
+                         | MenorIgual ExpressaoAritmetica ExpressaoAritmetica
+                         | Igual ExpressaoAritmetica ExpressaoAritmetica
+                         | Diferente ExpressaoAritmetica ExpressaoAritmetica
+                         deriving (Show)
+
+
+
+{-instance Show ExpressaoLogica where
+   show (BinarioLogico E a b) = "(" ++ show a ++ " && " ++ show b ++ ")"
+   show (Nao a) = "!" ++ show a
+   show Verdadeiro = "Verdadeiro" -}
--- Parser2.hs
+++ Parser2.hs
@@ -1,6 +1,6 @@
 import Text.Parsec
 import Text.Parsec.Expr
-import Text.Parsec.Token
+--import Text.Parsec.Token
 import Text.Parsec.Language
 import Data.Char
 import Head
@@ -14,21 +14,63 @@ parseFile a = do f <- readFile a
                  let e = parse expressaoAritmetica "Error:" (head ls)
                  return e
 
-expressaoAritmetica :: Parsec String () ExpressaoAritmetica
+--expressaoAritmetica :: Parsec String () ExpressaoAritmetica
 expressaoAritmetica = buildExpressionParser operadoresAritmeticos numero
 
-operadoresAritmeticos = [[Prefix (char '-' >> return (Neg))],
+operadoresAritmeticos :: [[Operator String u Identity ExpressaoAritmetica]]
+operadoresAritmeticos = [[Prefix (spaces >> char '-' >> spaces >> return (Neg))],
 
-                        [Infix  (char '*' >> return (Binario Multiplicacao)) AssocLeft,
-                         Infix  (char '/' >> return (Binario Divisao)) AssocLeft],
+                        [Infix  (spaces >> char '*' >> spaces >> return (Multiplicacao)) AssocLeft,
+                         Infix  (spaces >> char '/' >> spaces >> return (Divisao)) AssocLeft],
 
-                        [Infix  (char '+' >> return (Binario Adicao)) AssocLeft,
-                         Infix  (char '-' >> return (Binario Subtracao)) AssocLeft]]
+                        [Infix  (spaces >> char '+' >> spaces >> return (Adicao)) AssocLeft,
+                         Infix  (spaces >> char '-' >> spaces >> return (Subtracao)) AssocLeft]]
 
-{-bOperators = [ [Prefix (reservedOp "not" >> return (Not             ))          ]
-            , [Infix  (reservedOp "and" >> return (BBinary And     )) AssocLeft,
-               Infix  (reservedOp "or"  >> return (BBinary Or      )) AssocLeft]
-            ]-}
+operadoresLogicos :: [[Operator String u Identity ExpressaoLogica]]
+operadoresLogicos = [[Prefix (char '!' >> spaces >> return (Nao))],
+
+                     [Infix  (string "&&" >> spaces >> return (E)) AssocLeft,
+                      Infix  (string "||" >> spaces >> return (Ou)) AssocLeft]]
+
+--operadoresRelacionais :: [[Operator String u m ExpressaoRelacional]]
+operadoresRelacionais = (char '>' >> spaces >> return (Maior))
+                        <|>
+                        (char '<' >> spaces >> return (Menor))
+                        <|>
+                        (string ">=" >> spaces >> return (MaiorIgual))
+                        <|>
+                        (string "<=" >> spaces >> return (MenorIgual))
+                        <|>
+                        (string "==" >> spaces >> return (Igual))
+                        <|>
+                        (string "!=" >> spaces >> return (Diferente))
+
+expressaoRelacional = do try $ do l <- expressaoAritmetica
+                                  spaces
+                                  o <- operadoresRelacionais
+                                  r <- expressaoAritmetica
+                                  spaces
+                                  return (o l r)
+
+listaParametros = many1 parametros `sepBy` (char ',')
+
+parametros = literal <|> expressaoAritmetica
+
+identificador :: Parsec String () (Identificador)
+identificador = do s <- oneOf (['a'..'z'] ++ ['_'])
+                   ss <- many (oneOf (['a'..'z'] ++ ['A'..'Z'] ++ ['-','_']))
+                   return (s:ss)
+
+literal = undefined
 
 numero = do many1 digit
+            spaces
             return AA
+         <|>
+         do char '('
+            spaces
+            e <- expressaoAritmetica
+            spaces
+            char ')'
+            spaces
+            return e
