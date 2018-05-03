ba5389a4b55c8c869101a462132783821fdefa83
Gabriela Moreira <gabrielamoreira05@gmail.com>
2018-05-03 12:49:50 -0300

    TODO accept newlines

--- Head.hs
+++ Head.hs
@@ -1,29 +1,54 @@
 module Head where
 
-data Literal = String | Integer
+data Literal = Str String | Inte Integer deriving(Show)
 
 type Identificador = String
 
---data Programa = Completo ListaFuncoes BlocoPrincipal
-  --            | Main BlocoPrincipal
+data Tipo = Inteiro | String' | Float deriving(Show)
+
+data Retorno = R Tipo | Void deriving(Show)
+
+data Programa = Prog [DeclaracaoFuncao] BlocoPrincipal deriving(Show)
+
+data BlocoPrincipal = Main [Declaracao] Bloco deriving(Show)
+
+data DeclaracaoFuncao = Funcao Retorno Identificador DeclParametros BlocoPrincipal deriving(Show)
+
+data ChamadaFuncao = Chamada Identificador ListaParametros deriving(Show)
+
+type DeclParametros = [ParametroFormal]
+data ParametroFormal = ParamFormal Tipo Identificador deriving(Show)
 
 type ListaParametros = [Parametro]
 data Parametro = ParametroLiteral Literal
                | ParametroExpressao ExpressaoAritmetica
+               deriving(Show)
+
+data Declaracao = Decl Tipo [Identificador] deriving(Show)
+
+type Bloco = [Comando]
+
+data Comando = If ExpressaoLogica Bloco Bloco
+             | While ExpressaoLogica Bloco
+             | Atribui Identificador Parametro
+             | Escreve Parametro
+             | Le Identificador
+             | ChamadaProc ChamadaFuncao
+             | Ret Parametro
+             deriving(Show)
 
 data ExpressaoAritmetica = Multiplicacao ExpressaoAritmetica ExpressaoAritmetica
                          | Divisao ExpressaoAritmetica ExpressaoAritmetica
                          | Adicao ExpressaoAritmetica ExpressaoAritmetica
                          | Subtracao ExpressaoAritmetica ExpressaoAritmetica
                          | Neg ExpressaoAritmetica
-                         | AA
+                         | Numero Integer
                          deriving (Show)
 
 data ExpressaoLogica = E ExpressaoLogica ExpressaoLogica
                      | Ou ExpressaoLogica ExpressaoLogica
                      | Nao ExpressaoLogica
-                     | Verdadeiro
-                     | Falso
+                     | ER ExpressaoRelacional
                      deriving (Show)
 
 data ExpressaoRelacional = Maior ExpressaoAritmetica ExpressaoAritmetica
--- Parser.hs
+++ Parser.hs
@@ -1,5 +1,5 @@
 import Text.Parsec
-import Text.Parsec.Token
+import Text.Parsec.Expr
 import Text.Parsec.Language
 import Data.Char
 import Head
@@ -8,49 +8,213 @@ import Control.Monad.Identity (Identity)
 
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
+reservados = ",;.(){}"
+
 parseFile a = do f <- readFile a
-                 let ls = lines f
-                 let e = parse expressaoLogica "Error:" (head ls)
+                 let e = parse programa "Error:" (f)
                  return e
 
-reserv = "()->{}\n "
+--expressaoAritmetica :: Parsec String () ExpressaoAritmetica
+expressaoAritmetica = buildExpressionParser operadoresAritmeticos numero <|> numero
+
+operadoresAritmeticos :: [[Operator String u Identity ExpressaoAritmetica]]
+operadoresAritmeticos = [[Prefix (spaces >> char '-' >> spaces >> return (Neg))],
+
+                        [Infix  (spaces >> char '*' >> spaces >> return (Multiplicacao)) AssocLeft,
+                         Infix  (spaces >> char '/' >> spaces >> return (Divisao)) AssocLeft],
+
+                        [Infix  (spaces >> char '+' >> spaces >> return (Adicao)) AssocLeft,
+                         Infix  (spaces >> char '-' >> spaces >> return (Subtracao)) AssocLeft]]
+
+expressaoLogica = buildExpressionParser operadoresLogicos expressaoRelacional <|> expressaoRelacional
 
 
-expressaoLogica :: Parsec String () (ExpressaoLogica)
-expressaoLogica = (do try $ ouLogico) <|> (do try $ eLogico) <|> expressaoLogica'
+operadoresLogicos :: [[Operator String u Identity ExpressaoLogica]]
+operadoresLogicos = [[Prefix (char '!' >> spaces >> return (Nao))],
 
-expressaoLogica' :: Parsec String () (ExpressaoLogica)
-expressaoLogica' = (do try $ naoLogico) <|> (do try $ do pExpressaoLogica) <|> expressaoRelacional
+                     [Infix  (string "&&" >> spaces >> return (E)) AssocLeft,
+                      Infix  (string "||" >> spaces >> return (Ou)) AssocLeft]]
 
-pExpressaoLogica = do char '('
-                      spaces
-                      e <- expressaoLogica
-                      spaces
-                      char ')'
-                      spaces
-                      return e
+--operadoresRelacionais :: [[Operator String u m ExpressaoRelacional]]
+operadoresRelacionais = (char '>' >> spaces >> return (Maior))
+                        <|>
+                        (char '<' >> spaces >> return (Menor))
+                        <|>
+                        (string ">=" >> spaces >> return (MaiorIgual))
+                        <|>
+                        (string "<=" >> spaces >> return (MenorIgual))
+                        <|>
+                        (string "==" >> spaces >> return (Igual))
+                        <|>
+                        (string "!=" >> spaces >> return (Diferente))
 
-ouLogico = do l <- expressaoLogica'
-              spaces
-              string "||"
-              spaces
-              r <- expressaoLogica
-              spaces
-              return (Ou l r)
+expressaoRelacional = do try $ do l <- expressaoAritmetica
+                                  spaces
+                                  o <- operadoresRelacionais
+                                  r <- expressaoAritmetica
+                                  spaces
+                                  return (ER (o l r))
 
-eLogico = do l <- expressaoLogica'
+listaParametros = parametro `sepBy` (char ',')
+
+parametro = do {e <- expressaoAritmetica; spaces; return (ParametroExpressao e)}
+            <|>
+            do {l <- literal; spaces; return (ParametroLiteral l)}
+
+chamadaFuncao = do try $ do i <-identificador
+                            spaces
+                            char '('
+                            spaces
+                            ps <- listaParametros
+                            spaces
+                            char ')'
+                            spaces
+                            return (Chamada i ps)
+
+chamadaProc = do f <- chamadaFuncao
+                 char ';'
+                 spaces
+                 return (ChamadaProc f)
+
+cmdLeitura = do try $ do string "read("
+                         spaces
+                         i <- identificador
+                         char ')'
+                         spaces
+                         char ';'
+                         spaces
+                         return (Le i)
+
+cmdEscrita = do try $ do string "print("
+                         spaces
+                         p <- parametro
+                         char ')'
+                         spaces
+                         char ';'
+                         spaces
+                         return (Escreve p)
+
+cmdAtrib = do try $ do i <- identificador
+                       char '='
+                       spaces
+                       p <- parametro
+                       spaces
+                       char ';'
+                       spaces
+                       return (Atribui i p)
+
+cmdEnquanto = do try $ do string "while("
+                          spaces
+                          el <- expressaoLogica
+                          spaces
+                          char ')'
+                          b <- bloco
+                          return (While el b)
+
+cmdSe = do try $ do string "if"
+                    spaces
+                    char '('
+                    spaces
+                    el <- expressaoLogica
+                    spaces
+                    char ')'
+                    spaces
+                    b1 <- bloco
+                    string "else"
+                    spaces
+                    b2 <- bloco
+                    return (If el b1 b2)
+       <|>
+       do string "if"
+          spaces
+          char '('
+          spaces
+          el <- expressaoLogica
+          spaces
+          char ')'
+          spaces
+          b1 <- bloco
+          return (If el b1 [])
+
+retorno = do string "return "
              spaces
-             string "&&"
+             p <- parametro
              spaces
-             r <- expressaoLogica
-             spaces
-             return (E l r)
+             return (Ret p)
 
-naoLogico = do char '!'
-               spaces
-               f <- expressaoLogica
-               spaces
-               return (Nao f)
+comando = cmdSe <|> cmdEnquanto <|> cmdAtrib <|> cmdEscrita <|> cmdLeitura <|> chamadaProc <|> retorno
+listaCmd = many1 comando
 
-expressaoRelacional = do string "er"
-                         return Verdadeiro
+bloco = do char '{'
+           cs <- listaCmd
+           char '}'
+           spaces
+           return cs
+
+tipo = do {string "int"; spaces; return (Inteiro)}
+       <|>
+       do {string "string"; spaces; return (String')}
+       <|>
+       do {string "float"; spaces; return (Float)}
+
+void = do {string "void"; spaces; return Void}
+
+tipoRetorno = do {t <- tipo; return (R t)} <|> void
+
+declaracao = do try $ do t <- tipo
+                         spaces
+                         is <- listaId
+                         char ';'
+                         spaces
+                         return (Decl t is)
+
+blocoPrincipal = do char '{'
+                    ds <- many declaracao
+                    cs <- listaCmd
+                    char '}'
+                    spaces
+                    return (Main ds cs)
+
+parametroFormal = do t <- tipo
+                     i <- identificador
+                     return (ParamFormal t i)
+
+declParametros = parametroFormal `sepBy` (char ',')
+
+funcao = do try $ do tr <- tipoRetorno
+                     i <- identificador
+                     ps <- declParametros
+                     b <- blocoPrincipal
+                     return (Funcao tr i ps b)
+
+programa = do fs <- many funcao
+              b <- blocoPrincipal
+              return (Prog fs b)
+
+identificador :: Parsec String () (Identificador)
+identificador = do s <- oneOf (['a'..'z'] ++ ['_'])
+                   ss <- many (oneOf (['a'..'z'] ++ ['A'..'Z'] ++ ['-','_']))
+                   spaces
+                   return (s:ss)
+
+listaId = identificador `sepBy` (char ',')
+
+literal = do try $ do digits <- many1 digit
+                      let n = foldl (\x d -> 10*x + toInteger (digitToInt d)) 0 digits
+                      spaces
+                      return (Inte n)
+          <|>
+          do try $ do {cs <- many1 (noneOf reservados); return (Str cs)}
+
+numero = do try $ do digits <- many1 digit
+                     let n = foldl (\x d -> 10*x + toInteger (digitToInt d)) 0 digits
+                     spaces
+                     return (Numero n)
+         <|>
+         do char '('
+            spaces
+            e <- expressaoAritmetica
+            spaces
+            char ')'
+            spaces
+            return e
